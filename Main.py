2623cfdaac46d53439650a21dd5f1d528d035294
Lucas <40646477@live.napier.ac.uk>
2024-12-17 21:42:49 +0000

    Finished the roller

--- Main.py
+++ Main.py
@@ -1,10 +1,38 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, redirect
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('Home.html')
+
+@app.route("/Home")
+def home():
+    return render_template('Home.html')
+
+@app.route("/about")
+def about():
+    return render_template('Info.html')
+
+@app.route("/Login")
+def Login():
+    return render_template('Login.html')
+
+@app.route("/SignUp")
+def SignUp():
+    return render_template('SignUp.html')
+
+@app.route("/Roller", methods=['GET', 'POST'])
+def Roller():
+    if request.method == 'POST':
+        sides = request.form['sides']
+        roll = random.randint(1, int(sides))
+        returnMessage = str(roll)
+        return render_template('Roller.html', rmessage=returnMessage, sides=sides)
+    else:
+        return render_template('Roller.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
